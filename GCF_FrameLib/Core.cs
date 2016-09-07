a1b5bebb4af512b76d23818ca141bcbb51889409
gmono <gaozijian0411@foxmail.com>
2016-09-07 12:16:26 +0800

    多会话模式完成编写

--- GCF_FrameLib/Core.cs
+++ GCF_FrameLib/Core.cs
@@ -14,9 +14,11 @@ namespace GCF_FrameLib
     ///             invoke函数中检测包含且值为null时调用loadwebobject函数创建新实例
     /// 独立模式：一个这样的core对象中 file独立加载 不考虑文件表 加载时创建对象 不放入文件表 调用时直接从对象表中搜索
     /// 默认为独立模式
+    /// 文件表中的“文件名”不一定是真实文件名 但是直接加载的文件会以完整路径作为文件名
     /// </summary>
     public class Core
     {
+         static List<Core> multiobjs = new List<Core>();//多会话模式的对象表 为了对所有对象进行同步
         /// <summary>
         /// 初始化
         /// </summary>
@@ -24,16 +26,23 @@ namespace GCF_FrameLib
         public Core(bool ismultisession=false)
         {
             ismulti = ismultisession;
+            if(ismulti)
+            {
+                //如果是多会话模式就从空类表复制一份到对象所有的对象表
+                objs = new Dictionary<string, WebObject>(emptyclasses);
+                multiobjs.Add(this);//把自己加入对象表
+            }
         }
-        bool ismulti;//是否多会话
+        private bool ismulti;//是否多会话
         class WebObject
         {
             public Type type=null;//对象的type信息 多会话模式时用到
             public object obj=null;//目标对象
-            public Dictionary<string, MethodInfo> methods=null;//方法表 
-            public Dictionary<string, PropertyInfo> props=null;//属性表
+            public Dictionary<string, MethodInfo> methods=null;//方法表 多会话模式下为所有对象共享
+            public Dictionary<string, PropertyInfo> props=null;//属性表 多会话模式下为所有对象共享
         }
-        SortedSet<string> loadedfiles = new SortedSet<string>();//这是加载过的文件的集合 目前设计用不到 添加多会话模式时用到
+        static SortedSet<string> loadedfiles = new SortedSet<string>();//这是加载过的文件的集合 目前设计用不到 添加多会话模式时用到
+        static Dictionary<string, WebObject> emptyclasses = new Dictionary<string, WebObject>();//加载过的类集合 每个多会话模式对象创建时自动复制一份到自己的对象表
         /// <summary>
         /// 对象表 不管什么模式 每个Core对象总有一个对象表
         /// </summary>
@@ -44,7 +53,7 @@ namespace GCF_FrameLib
         /// <param name="classname"> 调用的对象的类名 包括命名空间</param>
         /// <param name="funname">函数名</param>
         /// <param name="pars">参数表 由此函数内部转换为具体类型 注意类型只支持基础数据类型以及实现了Parse静态方法的类型</param>
-        bool invoke(string classname,string funname,params string[] pars)
+        bool invoke(string classname,string funname, out object result,params string[] pars)
         {
             if(objs.ContainsKey(classname))
             {
@@ -59,7 +68,7 @@ namespace GCF_FrameLib
                         for(int i=pars.Length;i<parinfo.Length;i++)
                         {
                             ParameterInfo pinfo = parinfo[i];
-                            if (!pinfo.HasDefaultValue) return false;//只要有一个没有默认值就返回 意味着参数个数小于参数表参数个数时后面的必须全部有默认值
+                            if (!pinfo.HasDefaultValue) { result = null; return false; }//只要有一个没有默认值就返回 意味着参数个数小于参数表参数个数时后面的必须全部有默认值
                         }
                     }
                     //此处开始转换对象
@@ -73,20 +82,35 @@ namespace GCF_FrameLib
                         else
                         {
                             MethodInfo parsemet = ptype.GetMethod("Parse");
-                            if (parsemet == null) return false;//如果有一个参数解析函数没有就返回 调用不合法
+                            if (parsemet == null) { result = null; return false; }//如果有一个参数解析函数没有就返回 调用不合法
                             object p = parsemet.Invoke(null, new object[] { s });//解析字符串
-                            if (p == null) return false;//解析失败就返回 调用不合法
+                            if (p == null) { result = null; return false; }//解析失败就返回 调用不合法
                             //成功 加入参数表
                             mpars[index] = p;
                         }
                     }
-                    minfo.Invoke(obj.obj, mpars);//调用方法
+                    //此处为多会话模式的关键代码 如果可以调用方法 并且需要创建对象就创建对象
+                    if (obj.obj == null) createWebObject(obj);//如果对象没有实例就创建一个实例 理论上如果独立模式采用延迟创建方法这个代码也可以工作
+                    result=minfo.Invoke(obj.obj, mpars);//调用方法 返回对象
                     return true; 
                 }
             }
-            return false;
+            result = null; return false; 
         }
-        private void loadWebObject(WebObject obj)
+        /// <summary>
+        /// 创建一个webobject对象的实例
+        /// </summary>
+        /// <param name="obj">webobject对象</param>
+        private void createWebObject(WebObject obj)
+        {
+            obj.obj = Activator.CreateInstance(obj.type);
+        }
+        /// <summary>
+        /// 加载webobject
+        /// </summary>
+        /// <param name="obj">对象</param>
+        /// <param name="iscreate">是否创建实例</param>
+        private void loadWebObject(WebObject obj,bool iscreate)
         {
             Type t = obj.type;
             //加载方法
@@ -107,19 +131,41 @@ namespace GCF_FrameLib
                 if (val == null) continue;
                 obj.props.Add(p.Name, p);
             }
-            obj.obj = Activator.CreateInstance(t);//创建对象实例
+            if (iscreate) createWebObject(obj);//创建对象实例
         }
         public void loadtype(Type t)
         {
             WebObject obj = new WebObject();
             obj.type = t;//加入类型信息
-            loadWebObject(obj);//加载这个webobject
-            //把对象加入对象表
-            objs.Add(t.FullName, obj);//
+            if (ismulti)
+            {
+                loadWebObject(obj, false);//不创建实例
+                //多会话模式的处理
+                emptyclasses.Add(t.FullName,new WebObject() { type=obj.type});
+                //以上为将类型复制一个加入空类型表 以便以后创建的多会话对象复制到自身对象表
+                //以下为处理以创建多会话对象的对象表
+                foreach(var v in multiobjs)
+                {
+                    v.objs.Add(t.FullName, new WebObject() { type = obj.type });//加入已创建对象的对象表 其中包括自己（构造函数中添加的this）
+                }
+            }
+            else
+            {
+                loadWebObject(obj,true);//加载这个webobject  如果这里传入false则是采用延迟创建的策略 其他代码无需修改
+                                        //把对象加入对象表
+                objs.Add(t.FullName, obj);//
+            }
+
             
         }
-        public void loadmod(Assembly asm)
-        {
+        /// <summary>
+        /// 加载一个程序集
+        /// </summary>
+        /// <param name="asm">程序集</param>
+        /// <param name="name">程序集的filename</param>
+        public void loadmod(Assembly asm,string filename)
+        { 
+            if (ismulti && loadedfiles.Contains(filename)) return; //已经加载返回 三层防护
             Type[] ts = asm.GetTypes();
             foreach(var type in ts)
             {
@@ -130,21 +176,24 @@ namespace GCF_FrameLib
                     loadtype(type);
                 }
             }
-
+            if (ismulti) loadedfiles.Add(filename);//多会话模式加载后加入文件表
         }
         /// <summary>
         /// 加载一个模块
         /// </summary>
         /// <param name="file">模块数据来源</param>
         /// <returns>是否成功</returns>
-        public bool Load(Stream file)
+        public bool Load(Stream file,string filename)
         {
+            if (ismulti && loadedfiles.Contains(filename)) return true; //已经加载返回成功 二层防护
+            //以上为多会话检测
             try
             {
                 byte[] filedata = new byte[file.Length];
                 file.Read(filedata, 0, (int)file.Length);
                 var v = Assembly.Load(filedata);
-                loadmod(v);
+                loadmod(v,filename);
+
                 return true;
             }
             catch(Exception)
@@ -154,10 +203,13 @@ namespace GCF_FrameLib
         }
         public bool loadfile(string file)
         {
+            string filename = new FileInfo(file).FullName;
+            if (ismulti && loadedfiles.Contains(filename)) return true; //已经加载返回成功 一层防护
+
             try
             {
                 var v = Assembly.LoadFile(file);
-                loadmod(v);
+                loadmod(v,filename);
                 return true;
             }
             catch(Exception)
